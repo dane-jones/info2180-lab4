b5ac707ed725ec4c16971f5dc83847dbfa1c2ffa
Dane Jones <danejordanjones1234@gmail.com>
2022-11-07 23:44:04 -0500

    Completed exercise 1

---
+++ app.js
@@ -0,0 +1,17 @@
+<div class="container">
+          
+            <p>Search by Full Name or Alias.</p>
+            <form action="#" method="post">
+                <div class="search-area">
+                    <input type="text" name="query" id="query" placeholder="e.g. Captain America or Steve Rogers">
+                </div>
+                <button type="submit" class="btn">Search</button>
+            </form>
+        </div>
+    </section>
+    <section class="ResultDisplay">
+        <h2>RESULT</h2>
+        <hr>
+        <div class="result" id="result"></div>
+    </section>
+</body> 
